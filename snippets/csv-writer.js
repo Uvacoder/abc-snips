bffb7ed93e3c7d7a62a18d345e8f3e3d70a61a22
Reaper <ahoy@barelyhuman.dev>
2021-07-16 16:55:40 +0530

    chore: add csv-writer

---
+++ snippets/csv-writer.js
@@ -0,0 +1,22 @@
+const code = `
+function CSVWriter() {
+	this.csv = "";
+
+	this.addToLine = function (msg, { start } = {}) {
+		const prefix = start ? "" : ",";
+		this.csv += prefix + msg;
+		return this;
+	};
+
+	this.completeLine = function () {
+		this.csv += "\\n";
+		return this;
+	};
+
+	this.value = function () {
+		return this.csv;
+	};
+}
+`;
+
+export default code;
